f077186f724dbd9e8aaf25a11183d7cf84c3dff5
Luis Martínez <luchin2000@hotmail.com>
2022-10-13 23:09:34 -0400

    Pantallas del Tab5 y Tab6 del PTDI Indicador Resultado-programacionfisica

--- src/pages/ptdi/indexPtdi.vue
+++ src/pages/ptdi/indexPtdi.vue
@@ -41,7 +41,7 @@
 
     <a-tab-pane key="2" tab="2. SELECTOR PDES">
       <div>
-        <H6>Seleccionar PDES</H6>
+        <H6>Selector para PDES</H6>
         <a-tree
           v-model:expandedKeys="expandedKeys"
           v-model:selectedKeys="selectedKeys"
@@ -109,6 +109,7 @@
 
 
     <a-tab-pane key="4" tab="4. REGISTRO RESULTADO">
+      <H6>Seleccionar Datos de Resultados</H6>
       <a-form
         :model="formState"
         v-bind="layout2"
@@ -162,16 +163,15 @@
             <a-button type="primary" color:positive @click="showModal"><template #icon><SearchOutlined /></template>AGREGAR NUEVA DESCRIPCIÓN</a-button>
             <a-modal
               v-model:visible="visible"
+              height="1000px"
               title="Agregando una Nueva Descripción"
-              :ok-button-props="{ disabled: true }"
+              :ok-button-props="{ disabled: false }"
               :cancel-button-props="{ disabled: false }"
               @ok="handleOk"
             >
-            
               <a-form-item :name="['formtab4datoresultado', 'detalle']" label="DETALLE">
                 <a-textarea v-model:value="formState.formtab4datoresultado.detalle" />
               </a-form-item>
-
             </a-modal>
           </div>
         </a-form-item>
@@ -184,14 +184,151 @@
 
 
     <a-tab-pane key="5" tab="5. INDICADOR RESULTADO">
-      
+      <H6>Seleccionar los Datos del Indicador para Resultados</H6>
+      <a-form
+        :model="formState"
+        v-bind="layout3"
+        name="nest-messages"
+        :validate-messages="validateMessages"
+        @finish="onFinish"
+      >
+        <a-row>
+          <a-col :span="24">
+            <a-form-item align="center" :name="['formtab5datosindicadorresultado', 'encendidoindicardor']" label="INDICADOR ABSOLUTO (ENCENDIDO) / INDICADOR RELATIVO (APAGADO)">
+              <a-switch v-model:checked="checked" />
+            </a-form-item>
+            <div v-if="!checked">
+              <a-form-item :name="['formtab5datosindicadorresultado', 'tipoindicadorabsoluto']" label="TIPO DE INDICADOR ABSOLUTO" >
+                <a-select
+                  v-model:value="formState.formtab5datosindicadorresultado.tipoindicadorabsoluto"
+                  show-search
+                  placeholder="Seleccionar Indicador Absoluto"
+                  :options="options4"
+                  :filter-option="filterOption"
+                  @focus="handleFocus"
+                  @blur="handleBlur"
+                  @change="handleChange"
+                ></a-select>
+              </a-form-item>
+              <a-form-item :name="['formtab5datosindicadorresultado', 'indicadorresultadoabsoluto']" label="INDICADOR RESULTADO ABSOLUTO">
+                <a-textarea v-model:value="formState.formtab5datosindicadorresultado.indicadorresultadoabsoluto" />
+              </a-form-item>
+              <a-form-item :name="['formtab5datosindicadorresultado', 'formulaabsoluto']" label="FÓRMULA INDICADOR ABSOLUTO">
+                <a-input v-model:value="formState.formtab5datosindicadorresultado.formulaabsoluto" />
+              </a-form-item>
+            </div>
+            <div v-else-if="checked">
+              <a-form-item :name="['formtab5datosindicadorresultado', 'tipoindicadorrelativo']" label="TIPO DE INDICADOR RELATIVO" >
+                <a-select
+                  v-model:value="formState.formtab5datosindicadorresultado.tipoindicadorrelativo"
+                  show-search
+                  placeholder="Seleccionar Indicador Relativo"
+                  :options="options5"
+                  :filter-option="filterOption"
+                  @focus="handleFocus"
+                  @blur="handleBlur"
+                  @change="handleChange"
+                ></a-select>
+              </a-form-item>
+              <a-form-item :name="['formtab5datosindicadorresultado', 'indicadorresultadoabsoluto']" label="INDICADOR RESULTADO RELATIVO">
+                <a-textarea v-model:value="formState.formtab5datosindicadorresultado.indicadorresultadorelativo" />
+              </a-form-item>
+              <a-form-item :name="['formtab5datosindicadorresultado', 'formularelativo']" label="FÓRMULA INDICADOR RELATIVO">
+                <a-input v-model:value="formState.formtab5datosindicadorresultado.formularelativo" />
+              </a-form-item>
+              <a-form-item :name="['formtab5datosindicadorresultado', 'encendidodenominador']" label="DENOMINADOR VARIABLE (ENCENDIDO) / DENOMINADOR FIJO (APAGADO)">
+                <a-radio-group v-model:value="value3" name="radioGroup">
+                  <a-radio value="1">Denominador Variable</a-radio>
+                  <a-radio value="2">Denominador Fijo</a-radio>
+                </a-radio-group>
+              </a-form-item>
+            </div>
+          </a-col>
+        </a-row>
+
+        <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 8 }">
+          <a-button type="primary" html-type="submit">GUARDAR INDICADOR RESULTADO</a-button>
+        </a-form-item>
+      </a-form>
 
     </a-tab-pane>
 
 
 
-    <a-tab-pane key="6" tab="6. PROGRAMACIÓN FÍSICA">Content of Tab Pane 3</a-tab-pane>
+    <a-tab-pane key="6" tab="6. PROGRAMACIÓN FÍSICA">
+
+
+
+      <a-form
+        :model="formState"
+        v-bind="layout4"
+        name="nest-messages"
+        :validate-messages="validateMessages"
+        @finish="onFinish"
+      >
+        <a-form-item :name="['formtab6programacionfisica', 'lineabase2020']" label="LINEA BASE 2020" :rules="[{ type: 'number', min: 0, max: 100 }]">
+          <a-input-number v-model:value="formState.formtab6programacionfisica.lineabase2020" />
+        </a-form-item>
+        <a-form-item :name="['formtab6programacionfisica', 'metaal2025']" label="META AL 2025" :rules="[{ type: 'number', min: 0, max: 100 }]">
+          <a-input-number v-model:value="formState.formtab6programacionfisica.metaal2025" />
+        </a-form-item>
+        <a-divider style="border-color: #8DB600" dashed />
+        <div align="center">
+        <a-row>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">2021</a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">2022</a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">2023</a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">2024</a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">2025</a-col>
+        </a-row>
+        <a-row>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <a-form-item :name="['formtab6programacionfisica', 'linea2021']" :rules="[{ type: 'number', min: 0, max: 100 }]">
+              <a-input-number v-model:value="formState.formtab6programacionfisica.linea2021" />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <a-form-item :name="['formtab6programacionfisica', 'linea2022']" :rules="[{ type: 'number', min: 0, max: 100 }]">
+              <a-input-number v-model:value="formState.formtab6programacionfisica.linea2022" />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <a-form-item :name="['formtab6programacionfisica', 'linea2023']" :rules="[{ type: 'number', min: 0, max: 100 }]">
+              <a-input-number v-model:value="formState.formtab6programacionfisica.linea2023" />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <a-form-item :name="['formtab6programacionfisica', 'linea2024']" :rules="[{ type: 'number', min: 0, max: 100 }]">
+              <a-input-number v-model:value="formState.formtab6programacionfisica.linea2024" />
+            </a-form-item>
+          </a-col>
+          <a-col :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <a-form-item :name="['formtab6programacionfisica', 'linea2025']" :rules="[{ type: 'number', min: 0, max: 100 }]">
+              <a-input-number v-model:value="formState.formtab6programacionfisica.linea2025" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+
+      </div>
+      <a-divider style="border-color: #8DB600" dashed />
+        <a-form-item :name="['formtab6programacionfisica', 'lineabase2020']" label="LINEA BASE 2020" :rules="[{ type: 'number', min: 0, max: 100 }]">
+          <a-input-number v-model:value="formState.formtab6programacionfisica.lineabase2020" />
+        </a-form-item>
+      
+        <a-form-item :wrapper-col="{ ...layout.wrapperCol, offset: 8 }">
+          <a-button type="primary" html-type="submit">GUARDAR PROGRAMACIÓN FÍSICA</a-button>
+        </a-form-item>
+      </a-form>
+
+
+    </a-tab-pane>
+
+
+
     <a-tab-pane key="7" tab="7. TERRITORIALIZACIÓN">Content of Tab Pane 3</a-tab-pane>
+
+
+
     <a-tab-pane key="8" tab="8. PROGRAMACIÓN FINANCIERA">Content of Tab Pane 3</a-tab-pane>
   </a-tabs>
 </template>
@@ -224,6 +361,22 @@
       descripcion: '',
       detalle: '',
     },
+    formtab5datosindicadorresultado: {
+      tipoindicadorabsoluto: '',
+      indicadorresultadoabsoluto: '',
+      formulaabsoluto: '',
+      tipoindicadorrelativo: '',
+      indicadorresultadorelativo: '',
+      formularelativo: '',
+    },
+    formtab6programacionfisica: {
+        lineabase2020: undefined,
+        metaal2025: undefined,
+        metaal2025: undefined,
+        metaal2025: undefined,
+        website: '',
+        introduction: '',
+      },
   });
 
   const options = ref([{
@@ -450,6 +603,54 @@
     console.log(e);
     visible.value = false;
   };
+
+  const layout3 = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+  const options4 = ref([{
+    value: 'jack',
+    label: 'Jack 4',
+  }, {
+    value: 'lucy',
+    label: 'Lucy',
+  }, {
+    value: 'tom',
+    label: 'Tom',
+  }]);
+
+  const options5 = ref([{
+    value: 'jack',
+    label: 'Jack 5',
+  }, {
+    value: 'lucy',
+    label: 'Lucy',
+  }, {
+    value: 'tom',
+    label: 'Tom',
+  }]);
+
+  const checked = ref(false);
+
+  const checked2 = ref(true);
+
+  const value3 = ref('1');
+
+  const layout4 = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+
 
 </script>
 
